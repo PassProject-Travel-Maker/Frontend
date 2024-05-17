674697541f1af5509a6a30f81b6acb7df3fc80c1
minkyung7365 <minkyung7365@gmail.com>
2024-05-17 14:24:28 +0900

    [ADD]SignInView, SignUpView

--- src/components/member/views/SigninView.vue
+++ src/components/member/views/SigninView.vue
@@ -1,24 +1,16 @@
 <script setup></script>
 
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  -->
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <!-- <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" /> -->
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">로그인</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" action="#" method="POST">
         <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">ID</label>
           <div class="mt-2">
             <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
@@ -42,9 +34,9 @@
       </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
-        Not a member?
+        회원이 아니신가요?
         {{ ' ' }}
-        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">회원가입</a>
       </p>
     </div>
   </div>
