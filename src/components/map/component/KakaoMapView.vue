fa50e3820ee045617b676200ecc12100c790f55d
kwonja <kownja87@gmail.com>
2024-05-18 18:32:10 +0900

    feat : 지도, 마커 PanTo 구현

--- src/components/map/component/KakaoMapView.vue
+++ src/components/map/component/KakaoMapView.vue
@@ -4,8 +4,41 @@ import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
 import SelectOptionView from "@/components/map/component/SelectOptionView.vue";
 import {useCategoryMapStore} from '@/stores/map.js';
 import {storeToRefs} from 'pinia';
+import { onMounted, watch,ref } from "vue";
+const { VITE_KAKAO_MAP_API_KEY } = import.meta.env;
+
 const store = useCategoryMapStore();
-const {areas} = storeToRefs(store);
+const {areas,location} = storeToRefs(store);
+
+const map = ref();
+
+const onLoadKakaoMap = (mapRef) => {
+  map.value = mapRef;
+};
+
+watch(
+  () => areas.value,
+  () => {
+    panTo();
+  }
+);
+
+
+onMounted( ()=>{
+  const script = document.createElement("script");
+    script.src =
+      `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${VITE_KAKAO_MAP_API_KEY}`;
+    document.head.appendChild(script);
+})
+
+const panTo = () => {
+  if (map.value) {
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.value.panTo(new kakao.maps.LatLng(location.value.latitude, location.value.longitude));
+  }
+};
+
 
 </script>
 
@@ -14,7 +47,10 @@ const {areas} = storeToRefs(store);
 
      <SelectOptionView/>
 
-     <KakaoMap :lat="33.450705" :lng="126.570667" :draggable="true" 
+     <KakaoMap :lat="33.450705" :lng="126.570667" 
+     @onLoadKakaoMap="onLoadKakaoMap"
+     :draggable="true"
+     level=6
      width=95% height=80%  
      style="margin : 0 auto">
       <KakaoMapMarker
--- src/stores/map.js
+++ src/stores/map.js
@@ -1,12 +1,18 @@
 import { ref, } from 'vue'
 import { defineStore } from 'pinia'
 import { getGugunApi, getSidoApi,getAttractionApi } from '@/apis/map';
-
 export const useCategoryMapStore = defineStore('category', () => {
-  
+ 
+
+
 const sidos = ref([]);
 const guguns = ref([]);
 const areas = ref([]);
+const location =ref({
+  latitude : 0,
+  longitude : 0
+});
+
 
 const getSido = async() =>{
     const response = await getSidoApi();
@@ -24,9 +30,23 @@ const getAttraction = async(searchData) =>{
   const response = await getAttractionApi(searchData);
   console.log(response)
   areas.value = response.data;
+
+
+  //이동할 좌표의 중심값
+  let lat=0;
+  let lng=0;
+  areas.value.forEach( (area)=>{
+    lat +=area.latitude;
+    lng +=area.longitude;
+  })
+  lat = lat/areas.value.length;
+  lng = lng/areas.value.length;
+  console.log(lat + " " + lng);
+  location.value.latitude=lat;
+  location.value.longitude=lng;
+
 }
 
 
-
-  return { sidos,guguns, getSido,getGugun,areas,getAttraction }
+  return { sidos,guguns, getSido,getGugun,areas,getAttraction,location }
 })
