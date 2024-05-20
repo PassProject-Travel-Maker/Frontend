6f1b5729c73d43f8bb9353c717219256ffd277eb
minkyung7365 <minkyung7365@gmail.com>
2024-05-20 15:27:03 +0900

    [MOD]여행 계획 생성 leftView 디자인 수정

--- src/components/map/component/CategoryView.vue
+++ src/components/map/component/CategoryView.vue
@@ -29,20 +29,21 @@ const categories = ref({
 <template>
   <div class="w-full  px-2 mt-10 sm:px-0" style="width : 100%">
     <TabGroup>
-      <TabList class="flex space-x-1 rounded-xl bg-slate-800 /20 p-1">
+      <TabList class="inline-flex rounded-xl">
         <Tab
           v-for="category in Object.keys(categories)"
           as="template"
           :key="category"
           v-slot="{ selected }"
         >
-          <button
+        <button
             :class="[
-              'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+              'inline-block rounded-t-xl py-2.5 px-4 text-sm font-medium font-bold leading-5',
               'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+              'border-t border-l border-r border-gray-300',
               selected
-                ? 'bg-white text-blue-700 shadow'
-                : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+                ? 'bg-blue-400 text-white shadow'
+                : 'text-blue-500 hover:bg-white/[0.12] hover:text-white',
             ]"
           >
             {{ category }}
@@ -50,11 +51,13 @@ const categories = ref({
         </Tab>
       </TabList>
 
+
+
       <TabPanels class="">
         <TabPanel
           v-for="(posts, idx) in Object.values(categories)"
           :key="idx"
-          class="mt-10"
+          class=""
         >
         <div v-if="categories[Object.keys(categories)[idx]][0].type === 'category'">
             <KakaoMapView/>
