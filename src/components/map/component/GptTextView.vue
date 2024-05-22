0a535851e855b239cecffe16af8be1b0348f547e
minkyung <minkyung7365@gmail.com>
2024-05-22 11:00:15 +0900

    [ADD]gpt 응답 로딩 애니메이션 추가

--- src/components/map/component/GptTextView.vue
+++ src/components/map/component/GptTextView.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { storeToRefs } from "pinia";
 import { useGPTStore } from "@/stores/gpt";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const gptStore = useGPTStore();
-const { gptResponse } = storeToRefs(gptStore);
+const { gptResponse, lottieContainer, anim } = storeToRefs(gptStore);
 
 const mode=ref(false);
 console.log(gptResponse);
@@ -12,13 +12,25 @@ console.log(gptResponse);
 const change = () =>{
   mode.value= !mode.value;
 }
+
+watch(gptResponse, () => {
+  if (anim.value) {
+    anim.value.destroy();
+  }
+});
 </script>
 
 <template>
-  <div>
-    <div class="text_box" v-html="gptResponse" v-if="mode"></div>
-    <div  @click="change" v-else>button</div>
-    <!-- <div class="text_box">{{ gptResponse }}</div> -->
+  <!-- <div>
+    <div ref="lottieContainer"></div>
+    <div class="text_box" v-html="gptResponse" v-if="gptResponse">
+    </div>
+  </div> -->
+
+  <div class="gpt-container">
+    <div class="animation-div" ref="lottieContainer"></div>
+    <div class="text_box" v-html="gptResponse" v-if="gptResponse">
+    </div>
   </div>
 </template>
 
@@ -27,8 +39,30 @@ const change = () =>{
   width: 100%;
   height: 100%;
   padding: 30px;
+  /* border: 1px solid #e4e4e7;
+  border-radius: 5px;
+  overflow: auto; */
+}
+
+.gpt-container {
+  width: 100%;
+  height: 100%;
   border: 1px solid #e4e4e7;
   border-radius: 5px;
   overflow: auto;
+  margin-bottom: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.animation-div {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(1.5);
+  z-index: 10; /* text_box보다 높은 z-index 설정 */
 }
 </style>
--- src/stores/gpt.js
+++ src/stores/gpt.js
@@ -1,13 +1,38 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 import { postDataToGPTApi } from "@/apis/gptApi.js";
+import Lottie from "lottie-web";
+import animationData from "@/assets/anim1.json";
+
 export const useGPTStore = defineStore("gpt", () => {
   const gptText = ref('');
   const gptResponse = ref('<div class=md-body"> <md-block>');
 
+  const lottieContainer = ref(null);
+  const anim = ref(null);
+
   const postDataToGPT = async (data) => {
     console.log(data);
+
+    if(anim.value) {
+      anim.value.destroy();
+      anim.value = null;
+    }
+
+    anim.value = Lottie.loadAnimation({
+      container: lottieContainer.value,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+    });
+
     let response = await postDataToGPTApi(data);
+
+    if(anim.value) {
+      anim.value.destroy();
+      anim.value = null;
+    }
 
     //데이터 가공 함수
     
@@ -22,8 +47,7 @@ export const useGPTStore = defineStore("gpt", () => {
     console.log(gptText.value);
     console.log("gptResponse::::::");
     console.log(gptResponse.value);
-
   };
 
-  return { postDataToGPT, gptText, gptResponse };
+  return { postDataToGPT, gptText, gptResponse, lottieContainer, anim };
 });
