9f3c6fcd34dbf241e5ef5faa791e2b59219b4225
minkyung <minkyung7365@gmail.com>
2024-05-22 13:27:06 +0900

    [DEL]배민 폰트 삭제

--- src/stores/gpt.js
+++ src/stores/gpt.js
@@ -7,7 +7,7 @@ import { marked } from "marked";
 
 export const useGPTStore = defineStore("gpt", () => {
   const gptText = ref('');
-  const gptResponse = ref('<div class=md-body"> <md-block>');
+  const gptResponse = ref('');
 
   const lottieContainer = ref(null);
   const anim = ref(null);
@@ -35,15 +35,25 @@ export const useGPTStore = defineStore("gpt", () => {
       anim.value = null;
     }
 
-    //데이터 가공 함수
-    
-    gptText.value = response.data.replace(/\[\[/g, '<button>')
-    .replace(/\]\]/g, '</button>');
-    
-    //
-    gptResponse.value += gptText.value;
-    gptResponse.value += '</md-block> </div>';
-    
+    // 데이터 가공 함수
+    gptText.value = response.data
+      .replace(/\[\[/g, '<button class="gpt-button">')
+      .replace(/\]\]/g, '</button>');
+
+    // 마크다운을 HTML로 변환
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      // headerIds: false,
+      // tables: true,
+      breaks: true,
+      // pedantic: false,
+      sanitize: false,
+      // smartLists: true,
+      // smartypants: false
+    });
+    gptResponse.value = marked(gptText.value);
+
     console.log("gptText::::::");
     console.log(gptText.value);
     console.log("gptResponse::::::");
